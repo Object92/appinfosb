1280dca023f2dfb68c371c99851c13cd0e336289
孙志斌 <2673260609@qq.com>
2019-09-16 17:56:41 +0800

    *-

---
+++ progect/appinfosb/src/main/java/com/bdqn/appinfosb/AppinfosbApplication.java
@@ -0,0 +1,13 @@
+package com.bdqn.appinfosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppinfosbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppinfosbApplication.class, args);
+    }
+
+}
